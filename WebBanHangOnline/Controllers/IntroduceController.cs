c3da850535ca20a648582016a3a15a05b05acc79
thuong <tn58213@gmail.com>
2024-04-23 13:37:01 +0700

    up

---
+++ WebBanHangOnline/Controllers/IntroduceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class IntroduceController : Controller
+    {
+        
+        // GET: Introduce
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
